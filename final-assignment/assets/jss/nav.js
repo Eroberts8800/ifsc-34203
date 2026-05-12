1b6c5e153efbc6590bda78ba6f0a8a62571d2388
eroberts8800 <143098152+Eroberts8800@users.noreply.github.com>
2026-05-12 06:22:56 +0000

    rewrote js for nav toggle switch for PE

--- final-assignment/assets/jss/nav.js
+++ final-assignment/assets/jss/nav.js
@@ -1,6 +1,11 @@
 const toggle = document.querySelector('.nav-toggle');
 const menu = document.querySelector('nav ul');
-toggle.addEventListener('click', () => {
-    toggle.classList.toggle('open');
-    menu.classList.toggle('open');
-});
+
+if (toggle && menu)
+{
+    toggle.addEventListener('click', function()
+    {
+        toggle.classList.toggle('open');
+        menu.classList.toggle('open');
+    });
+}
